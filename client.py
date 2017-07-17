fdcc639d25edd2a07c853dbdca0c436557c8b284
Unknown <jjcomish@shaw.ca>
2017-07-17 12:32:47 -0700

    Basic Socket Client Code from assignment 1

---
+++ client.py
@@ -0,0 +1,9 @@
+#copied Socket Client segement 
+import socket
+s = socket.socket()
+host = socket.gethostname()
+port = 9999
+address = (host, port)
+msg = 'test 123'
+s.connect(address)
+s.send(msg)
