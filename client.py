a8542f94c45a0dc3eb0f5b02adfd446950045088
Juan Comish <jjcomish@shaw.ca>
2017-07-18 19:23:28 -0700

    Moved some more testfiles into my current workspace, implemented some buttons into the client's GUI plan on addingg all the other buttons and then adding actual functionality to these buttons

--- client.py
+++ client.py
@@ -8,8 +8,26 @@ import socket
 #s.connect(address)
 #s.send(msg)
 
-from GUI import Window, run
-#from testing import say
+from GUI import Window, Button, Font, ListButton, application
+from GUI.StdFonts import system_font
+from GUI.StdColors import red, black
+from testing import say
+
+cities = ["Detorit" , "Montreal", "Vancouver", "Victoria", "Calgary", "Edmonton", "Quebec City", "Ottawa", "Toronto", "Winipeg", "Churchill", "Saskatoon", "Regina", "Yellowknife", "Whitehorse", "Dawson City", "Fort Simson", "Iqaluit", "Resolute", "Fredrickton","Saint John", "Halifax", "Dartmouth", "St. Johns", "Grand Falls-Windsor", "Charlottetown", "Summerside"]
+
+def occupied_city_list(): # enables the list 
+    btn2.enabled = 1
+
+def empty_city_list():
+    btn4.enabled = 1
+
+def founded_city():
+    btn4.enabled = 0
+    btn2.enabled = 0
+
+def joined_city():
+    btn2.enabled = 0
+    btn4.enabled = 0
 
 class TestWindow(Window):
 
@@ -30,11 +48,40 @@ class TestWindow(Window):
         say(self.name, "Key up:", event)
         print
 
-win1 = TestWindow(title = "Chatcity!", 
+join_button = Button(position = (30, 30), 
+    title = "Join City", action = occupied_city_list, style = 'cancel')
+
+join_list_button = ListButton(position = (30, join_button.bottom + 30), 
+    titles = ["Move to %s" % i for i in cities], #need to add so only occupied cities are avaliable to be moved to
+    action = joined_city)
+
+# btn3 for creating a city
+
+btn3 = Button(position = (btn2.right + 30, 30),
+    title = "Create City", action = empty_city_list, style = 'cancel')
+
+#button 4 for list of not yet created cities
+
+btn4 = ListButton(position = (btn2.right + 30, btn3.bottom + 30), 
+    titles = ["Found %s" % i for i in cities], #need to add so only occupied cities are avaliable to be moved to
+    action = founded_city)
+
+
+win = Window(title = "List Button")
+
+window = TestWindow(title = "Chatcity!", 
     bounds = (50, 70, 1800, 800),
     auto_position = False)
-win1.name = "Win1"
-win1.show()
+
+btn2.enabled = 0
+btn4.enabled = 0
+
+window.add(join_button)
+window.add(btn2)
+window.add(btn3)
+window.add(btn4)
+window.show()
 
 
-run()
+
+application().run()
