b9b93a130dd86323b286a497b22e82491f3af5a0
Athenasko <austinlee.uvic@gmail.com>
2017-07-20 20:03:01 -0700

    Switching to Linux
    Switching to Linux

--- client.py
+++ client.py
@@ -1,9 +1,19 @@
 #copied Socket Client segement 
 import socket
-s = socket.socket()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 9999
+
 address = (host, port)
-msg = raw_input("Please input your message: ")
-s.connect(address)
-s.send(msg)
+
+while True:
+	s.connect(address)
+	#try:
+	#	alias = raw_input("Please input an alias: ")
+	#except :
+	#	alias = host
+
+	msg = raw_input("Please input your message: ")
+	#s.send(alias)
+	s.send(msg)
--- server.py
+++ server.py
@@ -1,15 +1,44 @@
 #Copied Socket server segement
+import select
 import socket
-s = socket.socket()
+import sys
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 9999
 address = (host, port)
+user_list = []
+user_list.append(s)
 print('Starting Server...')
 s.bind(address)
-s.listen(5)
+s.listen(20)
 while True:
-    client, addr = s.accept()
-    data = client.recv(1024)
-    if data == "Quit":
-    	break
-    print('%s:%s says >> %s' % (addr[0], addr[1], data))
+	ready_to_read, ready_to_write, in_error = select.select(user_list, [], [])
+	for sock in ready_to_read:
+	 	if sock == s:
+	 	    client, addr = s.accept()
+    		data = client.recv(4096)
+     		if data == "Quit":
+     			break
+     		print('%s:%s says >> %s' % (addr[0], addr[1], data))
+     		#messageBroadcast(s, sock, data)
+     	else:
+     		data = sock.recv(4096)
+     		if data:
+     			print('%s:%s says >> %s' % (addr[0], addr[1], data))
+     			#messageBroadcast(s, sock, data)
+     		else:
+     			user_list.remove(sock)
+    #client, addr = s.accept()
+    #alias = client.recv(1024)
+    #data = client.recv(1024)
+    #if data == "Quit":
+    #	break
+    #if alias:
+    #	print('%s says >> %s' % (alias, data))
+    #else:
+	#print('%s:%s says >> %s' % (addr[0], addr[1], data))
+
+def messageBroadcast (s, sock, data):
+	for socket in user_list:
+		if socket != s and socket != sock:
+			socket.send(data)
