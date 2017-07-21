b5c417e10a7257c9f4f3c0117ed4a5e12d7358c5
Juan Comish <jjcomish@shaw.ca>
2017-07-20 19:49:32 -0700

    Figured out chat room buttons, able to connect this eventually to actual chat room functionality, moving on to implementing dialoge box for sending messages

--- client.py
+++ client.py
@@ -13,25 +13,24 @@ from GUI.StdFonts import system_font
 from GUI.StdColors import red, black
 from testing import say
 
-free_room_counter = 1
+free_cities = ["Detorit" , "Montreal", "Vancouver", "Victoria", "Calgary", "Edmonton", "Quebec City", "Ottawa", "Toronto", "Winipeg", "Churchill", "Saskatoon", "Regina", "Yellowknife", "Whitehorse", "Dawson City", "Fort Simson", "Iqaluit", "Resolute", "Fredrickton", "Saint John", "Halifax", "Dartmouth", "St. Johns", "Grand Falls-Windsor", "Charlottetown", "Summerside"]
 
-free_cities = {1:"Detorit" , 2:"Montreal", 3:"Vancouver", 4:"Victoria", 5:"Calgary", 6:"Edmonton", 7:"Quebec City", 8:"Ottawa", 9:"Toronto", 10:"Winipeg", 11:"Churchill", 12:"Saskatoon", 13:"Regina", 14:"Yellowknife", 15:"Whitehorse", 16:"Dawson City", 17:"Fort Simson", 18:"Iqaluit", 19:"Resolute", 20:"Fredrickton", 21:"Saint John", 22:"Halifax", 23:"Dartmouth", 24:"St. Johns", 25:"Grand Falls-Windsor", 26:"Charlottetown", 27:"Summerside"}
-
-taken_cities = {}
+taken_cities = []
 
 current_city = "Earth"
 
 def founded_city(): #possibly consider making a function for disabling buttons to reduce code copyingg
     global free_room_counter
-    current_city = free_cities[create_list_button.value + free_room_counter]
+    global free_cities
+    global taken_cities
+    current_city = free_cities[create_list_button.value]
     print "Founded the city of", current_city
-    taken_cities[len(taken_cities)+1] = current_city
-    del free_cities[create_list_button.value + free_room_counter]
-    free_room_counter = free_room_counter + 1
+    taken_cities.append(current_city)
+    del free_cities[create_list_button.value]
     remove_window()
     refresh_buttons()
-    print taken_cities
-    print free_cities
+    #DEBUG print taken_cities
+    #DEBUG print free_cities
     create_list_button.enabled = 0
     join_list_button.enabled = 0
     join_button.enabled = 0
@@ -39,7 +38,9 @@ def founded_city(): #possibly consider making a function for disabling buttons t
 
 def joined_city():
     global free_room_counter
-    current_city = taken_cities[join_list_button.value + 1]
+    global free_cities
+    global taken_cities
+    current_city = taken_cities[join_list_button.value]
     print "Moved to the city of", current_city
     remove_window()
     refresh_buttons()
@@ -47,8 +48,8 @@ def joined_city():
     create_list_button.enabled = 0
     join_button.enabled = 0
     create_button.enabled = 0
-    print taken_cities
-    print free_cities
+    #DEBUG print taken_cities
+    #DEBUG print free_cities
 
 def empty_city_list():
     create_list_button.enabled = 1
@@ -66,23 +67,23 @@ join_button = Button(position = (30, 30),
         style = 'cancel')
 
 join_list_button = ListButton(position = (30, join_button.bottom + 30), 
-    titles = ["Move to %s" % taken_cities[i] for i in taken_cities], #need to add so only occupied free_cities are avaliable to be moved to
+    titles = ["Move to %s" % i for i in taken_cities], #need to add so only occupied free_cities are avaliable to be moved to
     action = joined_city)
 
     # create_button for creating a city
 
-create_button = Button(position = (join_list_button.right + 30, 30),
+create_button = Button(position = (300, 30),
     title = "Create City", 
     action = empty_city_list, 
     style = 'cancel')
 
     #button 4 for list of not yet created free_cities
 
-create_list_button = ListButton(position = (join_list_button.right + 30, create_button.bottom + 30), 
-    titles = ["Found %s" % free_cities[i] for i in free_cities], #need to add so only occupied free_cities are avaliable to be moved to
+create_list_button = ListButton(position = (300, create_button.bottom + 30), 
+    titles = ["Found %s" % i for i in free_cities], #need to add so only occupied free_cities are avaliable to be moved to
     action = founded_city)
 
-leave_button = Button(position = (create_list_button.right + 30, 30), 
+leave_button = Button(position = (600, 30), 
     title = "Leave City", 
     action = leave_city, 
     style = 'cancel') 
@@ -96,6 +97,8 @@ def refresh_buttons():
     global create_button
     global create_list_button
     global leave_button
+    global free_cities
+    global taken_cities
 
     join_button = Button(position = (30, 30), 
         title = "Join City", 
@@ -103,23 +106,23 @@ def refresh_buttons():
         style = 'cancel')
 
     join_list_button = ListButton(position = (30, join_button.bottom + 30), 
-        titles = ["Move to %s" % taken_cities[i] for i in taken_cities], #need to add so only occupied free_cities are avaliable to be moved to
+        titles = ["Move to %s" % i for i in taken_cities], #need to add so only occupied free_cities are avaliable to be moved to
         action = joined_city)
 
     # create_button for creating a city
 
-    create_button = Button(position = (join_list_button.right + 30, 30),
+    create_button = Button(position = (300, 30),
         title = "Create City", 
         action = empty_city_list, 
         style = 'cancel')
 
     #button 4 for list of not yet created free_cities
 
-    create_list_button = ListButton(position = (join_list_button.right + 30, create_button.bottom + 30), 
-        titles = ["Found %s" % free_cities[i] for i in free_cities], #need to add so only occupied free_cities are avaliable to be moved to
+    create_list_button = ListButton(position = (300, create_button.bottom + 30), 
+        titles = ["Found %s" % i for i in free_cities], #need to add so only occupied free_cities are avaliable to be moved to
         action = founded_city)
 
-    leave_button = Button(position = (create_list_button.right + 30, 30), 
+    leave_button = Button(position = (600, 30), 
         title = "Leave City", 
         action = leave_city, 
         style = 'cancel')   
