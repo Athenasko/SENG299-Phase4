de6726e2c838d8a17abe3757c57dab8b376b00d4
Juan Comish <jjcomish@shaw.ca>
2017-07-23 14:29:02 -0700

    Figured out multithreading and got it to work for the client code. Need to figure out how to multithread the client with the GUI, and send data between the two threads.

--- client.py
+++ client.py
@@ -3,14 +3,27 @@
 import sys
 import socket
 import select
+import threading
+import time
  
-def chat_client():
-	if(len(sys.argv) < 3):
-		print 'Usage : python chat_client.py hostname port'
-		sys.exit()
+class clientThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      print "Starting " + self.name
+      chat_client("localhost",6969)
+      print "Exiting " + self.name
 
-	host = sys.argv[1]
-	port = int(sys.argv[2])
+def chat_client(host,port):
+	#if(len(sys.argv) < 3):
+	#	print 'Usage : python chat_client.py hostname port'
+	#	sys.exit()
+
+	#host = sys.argv[1]
+	#port = int(sys.argv[2])
      
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_socket.settimeout(2)
@@ -49,6 +62,13 @@ def chat_client():
 				server_socket.send(message_data)
 				sys.stdout.write('[Me] '); sys.stdout.flush() 
 
-if __name__ == "__main__":
 
-	sys.exit(chat_client())
+thread1 = clientThread(1, "Thread-1", 1)
+thread2 = clientThread(2, "Thread-2", 2)
+
+thread1.start()
+thread2.start()
+
+#if __name__ == "__main__":
+
+#	sys.exit(chat_client())
