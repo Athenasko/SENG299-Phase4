5a61e9c1f9fa32a16bb4dddd979149f438efb6fa
Juan Comish <jjcomish@shaw.ca>
2017-07-23 14:57:40 -0700

    Figuring out threading the client while running the gui in the main thread, need to check for bug fixing in the server and then attempt to send data between the two threads.

--- client.py
+++ client.py
@@ -10,6 +10,7 @@
 import socket
 import os, sys
 import threading
+import select
 from GUI import Window, Button, Font, ListButton, application, TextField, View, Image
 from GUI.Geometry import offset_rect, rect_sized
 from GUI.StdFonts import system_font
@@ -29,7 +30,61 @@ class guiThread (threading.Thread):
       print "Exiting " + self.name
 
 
+class clientThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      print "Starting " + self.name
+      chat_client("localhost",6969)
+      print "Exiting " + self.name
 
+def chat_client(host,port):
+	#if(len(sys.argv) < 3):
+	#	print 'Usage : python chat_client.py hostname port'
+	#	sys.exit()
+
+	#host = sys.argv[1]
+	#port = int(sys.argv[2])
+     
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_socket.settimeout(2)
+     
+	#connect to remote host
+	try:
+		server_socket.connect((host, port))
+	except:
+		print 'Unable to connect'
+		sys.exit()
+     
+	print 'Connected to remote host. You can start sending messages'
+	sys.stdout.write('[Me] '); sys.stdout.flush()
+     
+	while 1:
+		socket_list = [sys.stdin, server_socket]
+         
+		#Get the list sockets which are readable
+		ready_for_reading, ready_for_writing, error_condition = select.select(socket_list , [], [])
+         
+		for sock in ready_for_reading:             
+			if sock == server_socket:
+				#incoming message from remote server, server_socket
+				data = sock.recv(4096)
+				if not data:
+					print '\nDisconnected from chat server'
+					sys.exit()
+				else:
+					#print data
+					sys.stdout.write(data)
+					sys.stdout.write('[Me] '); sys.stdout.flush()     
+            
+			else:
+				#user entered a message
+				message_data = sys.stdin.readline()
+				server_socket.send(message_data)
+				sys.stdout.write('[Me] '); sys.stdout.flush() 
 
 
 def clientgui():
@@ -415,10 +470,12 @@ def clientgui():
 
 #clientgui()
 
-thread1 = guiThread(1, "Thread-1", 1)
-#thread2 = guiThread(2, "Thread-2", 2)
+thread1 = clientThread(1, "Thread-1", 1)
+thread2 = clientThread(2, "Thread-2", 2)
 
 thread1.start()
-#thread2.start()
+thread2.start()
 
+#clientgui()
 
+print "Exiting main thread"
