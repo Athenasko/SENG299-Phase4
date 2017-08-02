639747544d492b8b952b668dcf7ddbfb92a96379
Juan Comish <jjcomish@shaw.ca>
2017-08-02 16:31:59 -0700

    Cleaned up some old commented out code and added some documentation

--- clientgui.py
+++ clientgui.py
@@ -19,9 +19,6 @@ from GUI.StdFonts import system_font
 from GUI.StdColors import red,black,yellow,blue 
 from testing import say
 
-testlock = threading.Lock()
-testlock2 = threading.Lock()
-threadtest = 2
 supertestcounter = 1
 sending = 0
 received = ""
@@ -31,23 +28,7 @@ line_counter = 0
 received_city = ""
 received_alias = ""
 
-#datapattern = re.complie( )# pattern for seperating the data string into key parts 
-
-#THREAD CLASSES FOR ATTEMPT TO MULTITHREAD - DOES NOT WORK DUE TO PYTHON GIL
-
-class guiThread (threading.Thread): #Just going to run in the main thread
-   def __init__(self, threadID, name, counter):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-      self.counter = counter
-   def run(self):
-      print "Starting " + self.name
-      clientgui()
-      print "Exiting " + self.name
-
-
-class clientThread (threading.Thread):
+class clientThread (threading.Thread): #Thread for running client - server communication behind GUI
   def __init__(self, threadID, name, counter):
     threading.Thread.__init__(self)
     self.threadID = threadID
@@ -58,24 +39,8 @@ class clientThread (threading.Thread):
     chat_client("localhost",6969)
     print "Exiting " + self.name
 
-class dummyThread (threading.Thread):
-  def __init__(self, threadID, name, counter):
-    threading.Thread.__init__(self)
-    self.threadID = threadID
-    self.name = name
-    self.counter = counter
-  def run(self):
-    print "Starting " + self.name
-    dummyfunction()
-    print "Exiting " + self.name
 
-def dummyfunction():
-  global supertestcounter
-  while(1):
-    supertestcounter += 1
-    if supertestcounter >= 2000000:
-      break
-  supertestcounter = "OOOOOH YEAH I'M MR MEESEEKS LOOK AT ME"
+
 
 #CHAT CLIENT FOR BOGO TO BOGO CONNECTION TO SERVER
 
@@ -89,11 +54,11 @@ def chat_client(host,port):
   global received
   global received_city
   global received_alias
-  #if(len(sys.argv) < 3):
+  
+  #if(len(sys.argv) < 3): #MIGHT STILL WANT ALL OF THIS
   # print 'Usage : python chat_client.py hostname port'
   # sys.exit()
-
-  #host = sys.argv[1]
+  #host = sys.argv[1] #MIGHT STILL WANT TO IMPLEMENT THIS FOR SYSTEM 
   #port = int(sys.argv[2])
      
   server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -107,70 +72,41 @@ def chat_client(host,port):
     sys.exit()
      
   print 'Connected to remote host. You can start sending messages'
-  #sys.stdout.write('[Me] '); sys.stdout.flush()
-  #print "Lose control of the interperator right about"
   
-  socket_list = [sys.stdin, server_socket] # replace with code from binary tides
+  socket_list = [sys.stdin, server_socket]
   
 
   while 1:
-    #socket_list = [sys.stdin, server_socket]
     socket_list = [server_socket]
     supertestcounter += 1
-    #print socket_list[0].family
     #Get the list sockets which are readable
-    ready_for_reading, ready_for_writing, error_condition = select.select(socket_list , [], [], 0)
-    #print socket_list     
+    ready_for_reading, ready_for_writing, error_condition = select.select(socket_list , [], [], 0)  
     for sock in ready_for_reading:  
-      threadtest = 0           
-      if sock == server_socket:
-        #incoming message from remote server, server_socket
+
+      if sock == server_socket: #incoming message from remote server, server_socket
         data = sock.recv(4096)
         if not data:
-          #print '\nDisconnected from chat server' # Maybe add this as an error to be caught by the system if the user tries to send a message after they've been disconected
           sys.exit()
         else:
-          #call function on the main thread that fixes things
-          tempdata = data
-          received = re.findall('\((.*?)\)',tempdata) 
-          tempdata = data
-          received_city = re.findall('\{(.*?)\}',tempdata)
-          tempdata = data
-          received_alias = re.findall('\[(.*?)\]',tempdata)
           
-
+          tempdata = data
+          received = re.findall('\((.*?)\)',tempdata) # core message
+          tempdata = data
+          received_city = re.findall('\{(.*?)\}',tempdata) # message city
+          tempdata = data
+          received_alias = re.findall('\[(.*?)\]',tempdata) # message alias 
+    
           refresh_output_receive()
-          pass
-          #print data
-          #sys.stdout.write(data) #THIS NEEDS TO BE WHERE WE INCORPORATE THE TRANSFERING OF THE DATA FROM THE SERVER TO THE MESSAGE BOX
-          #sys.stdout.write('[Me] '); sys.stdout.flush()     
+          pass 
             
       else:
+  
+        pass 
 
-     
-        pass
-
-    threadtest = 0    
-
-    if sending == 1:
-
-      #user entered a message
+    if sending == 1: #user entered a message
       message_data = '{' + current_city + '}' +  '[' + current_alias + ']' + '(' + window.input_field.text + ')' #+ '\n'
-      #message_data = [current_alias, current_city,window.input_field.text]
-      #message_data.append
-      #sys.stdin.readline() # THIS IS WERE WE NEED TO INCORPPORATE THE TRANSFERING OF THE DATA FROM THE TEXT SEND TO BE SENT TO THE SERVER
       server_socket.send(message_data)
       refresh_output_send()
-
-      #window.output_field.value = window.output_field.text + '[' + current_alias + '] ' +  window.input_field.text + '\n' # ACTUALLY WORKING Dummy CODE
-      #window.output_field.value = window.output_field.text + " " + str(test_counter) + "\n" # + latest output #DEBUG CODE
-      #print window.output_field.value
-      #remove_window()
-      #create_window()
-
-
-        #message_date =  # either an array with each item or, figure out how to send the username, cityname, messagge
-        #server_socket.send(messageg_data)
       sending = 0;
 
 
@@ -180,17 +116,10 @@ def chat_client(host,port):
 
 #list of all the different chat rooms - 2 of each city per province + Detroit
 free_cities = ["Detroit" , "Montreal", "Vancouver", "Victoria", "Calgary", "Edmonton", "Quebec City", "Ottawa", "Toronto", "Winnipeg", "Churchill", "Saskatoon", "Regina", "Yellowknife", "Whitehorse", "Dawson City", "Fort Simson", "Iqaluit", "Resolute", "Fredericton", "Saint John", "Halifax", "Dartmouth", "St. Johns", "Grand Falls-Windsor", "Charlottetown", "Summerside"]
-#taken_cities = [] #REMOVED CREATION OF CHATROOM FUNCTIONALITY DUE TO COMPLICATIONS WITH UPDATING LISTS ON ALL CLIENTS
-
-Alias_MAX = 10 #Need to test and fix values
-Message_MAX = 200 #aswell 
-
+Alias_MAX = 10 #
 test_counter = 1
-
-current_city = "Earth"
-
-#Need to implement a better default alias and system for changing it - going to attempt to use dialog boxes
-current_alias = "Anonymus" 
+current_city = "Earth" #Default chatroom for the system, also acts as global message sender
+current_alias = "Anonymus" #Default alias for the alias system
 
 
 #ALL THE PATHING FOR SWITCHING THE BACKGROUND FOR EACH CHAT ROOM
@@ -225,27 +154,8 @@ winnipeg_path = os.path.join(here, "winnipeg_background.jpg")
 yellowknife_path = os.path.join(here, "yellowknife_background.jpg")
 background_image = Image(file = image_path)
 
-#Removed functionality of creatin chat rooms
-#def founded_city(): #possibly consider making a function for disabling buttons to reduce code copyingg
-#    global free_room_counter
-#    global free_cities
-#    global taken_cities
-#   global current_city
-#   current_city = free_cities[create_list_button.value]
-    #print "Founded the city of", current_city
-#    switch_background()
-#    taken_cities.append(current_city)
-#    del free_cities[create_list_button.value]
-#    remove_window()
-#    refresh_buttons()
-    #DEBUG print taken_cities
-    #DEBUG print free_cities
-#    create_list_button.enabled = 0
-#    join_list_button.enabled = 0
-#    join_button.enabled = 0
-#    create_button.enabled = 0
-#    leave_button.enabled = 1
 
+#ALL THE CORE FUNCTIONS FOR BUTTON USAGE
 
 def joined_city(): #Joining chatrooms
     global free_room_counter
@@ -254,21 +164,13 @@ def joined_city(): #Joining chatrooms
     global current_city
     global line_counter
     current_city = free_cities[join_list_button.value]
-    #print "Moved to the city of", current_city
     switch_background()
     remove_window()
     refresh_buttons()
     join_list_button.enabled = 0
     line_counter = 0
-#    create_list_button.enabled = 0
     join_button.enabled = 0
-#    create_button.enabled = 0
     leave_button.enabled = 1
-    #DEBUG print taken_cities
-    #DEBUG print free_cities
-
-#def empty_city_list():
-#    create_list_button.enabled = 1
 
 def occupied_city_list(): # enables the list button for changing rooms
     join_list_button.enabled = 1
@@ -297,16 +199,12 @@ def send_message(): #need to implement
     #remove_window()
     
     if len(window.input_field.text) > MAX_SEND_LEN:
-      print("Message too long")
+      print("Message too long") #will want to move to error dialog box 
     elif line_counter > MAX_LINE:
       pass
     else:
       sending = 1
       #refresh_output_send()
-      
-    #print "Debug " 
-   # print window.input_field.text
-   # print window.input_field
 
 def change_alias(): #need to implement 
   global current_alias
@@ -326,7 +224,6 @@ def switch_background():
     global current_city
     global view
     global background_image
-   # print current_city
     if current_city == "Calgary":
        background_image = Image(file = calgary_path)
     elif current_city == "Charlottetown":
@@ -392,18 +289,10 @@ def test(): #Testing the sending of shared data between the threads - this is wh
   global received_alias
   global received_city
   global received
-  #testlock.acquire()
-  #if threadtest != 0:
-  # threadtest = 1
-  #testlock.release()
   print supertestcounter
   print received_alias
   print received_city
   print received
-#   if threadtest == 0:
-#     print "DEBUG HELLO"
-
-    #view = ImageView(size = window.size)
 
 def refresh_output_receive():
     global received
@@ -413,7 +302,6 @@ def refresh_output_receive():
     global line_counter
     global received_city
     global received_alias
-    #This would be where I would implement the filtering of the received text before refreshing the output 
     line_counter += 1
     print line_counter
     if line_counter >= MAX_LINE:
@@ -429,31 +317,23 @@ def refresh_output_receive():
         else:
           pass
 
-#Would call refresh_output_send everytime the 
-def refresh_output_send(): #going to require this to constantly feed the output from the server to the user
+def refresh_output_send():
     global received
     global window
     global test_counter
     global MAX_LINE
     global line_counter
-   # print "Debug"
-    #print window.output_field.value
-    #print "DEBUG"
-    #window.output_field.value = window.output_field.text + received
     line_counter += 1
     print line_counter
     if line_counter >= MAX_LINE:
       print "Too many lines" # - same thing as the recieving
     else:
       window.output_field.value = window.output_field.text + '[' + current_alias + '] ' +  window.input_field.text + '\n'
-      #window.input_field.value = ""
-    #window.output_field.value = window.output_field.text + " " + str(test_counter) + "\n" # + latest output #DEBUG CODE
-    #print window.output_field.value
     remove_window()
     create_window()
-    #test_counter += 1
-   #pass
 
+
+#ALL THE BUTTONS 
 
 join_button = Button(position = (30, 30), 
         title = "Join City", 
@@ -463,15 +343,6 @@ join_button = Button(position = (30, 30),
 join_list_button = ListButton(position = (30, join_button.bottom + 30), 
     titles = ["Move to %s" % i for i in free_cities], #need to add so only occupied free_cities are avaliable to be moved to
     action = joined_city)
-
-#create_button = Button(position = (300, 30), # create_button for creating a city
-#    title = "Create City", 
-#    action = empty_city_list, 
-#    style = 'cancel')
-
-#create_list_button = ListButton(position = (300, create_button.bottom + 30), #button 4 for list of not yet created free_cities
-#    titles = ["Found %s" % i for i in free_cities], #need to add so only occupied free_cities are avaliable to be moved to
-#    action = founded_city)
 
 leave_button = Button(position = (600, 30), 
     title = "Leave City", 
@@ -495,41 +366,10 @@ mail_button = Button(position = (600, alias_button.bottom + 30),
  style = 'cancel')
 
 join_list_button.enabled = 0
-#create_list_button.enabled = 0
 leave_button.enabled = 0
 
-#def refresh_buttons(): #REMOVED WITH LIST SWITCHING 
-#    global join_button
-#    global join_list_button
-#    global create_button
-#    global create_list_button
-#    global leave_button
-#    global free_cities
-#    global taken_cities
-#    global window
 
-#    join_button = Button(position = (30, 30), 
-#        title = "Join City", 
-#        action = occupied_city_list, 
-#        style = 'cancel')
-
-#    join_list_button = ListButton(position = (30, join_button.bottom + 30), 
-#        titles = ["Move to %s" % i for i in free_cities], #need to add so only occupied free_cities are avaliable to be moved to
-#        action = joined_city)
-
-    #create_button = Button(position = (300, 30),
-    #    title = "Create City", 
-    #    action = empty_city_list, 
-    #    style = 'cancel')
-
-    #create_list_button = ListButton(position = (300, create_button.bottom + 30), 
-    #    titles = ["Found %s" % i for i in free_cities], #need to add so only occupied free_cities are avaliable to be moved to
-    #    action = founded_city)
-
-#    leave_button = Button(position = (600, 30), 
-#        title = "Leave City", 
-#        action = leave_city, 
-#        style = 'cancel')   
+#REFRESH FUNCTIONS 
 
 def refresh_buttons():
 
@@ -588,6 +428,8 @@ def refresh_buttonsv2():
 
   create_window() 
 
+# WINDOW VIEWS AND REFRESH FUNCTIONS
+
 class TestTextField(TextField):
 
     def __init__(self, number, *args, **kwds):
@@ -601,20 +443,13 @@ class ImageView(View):
         c.erase_rect(r)
         main_image_pos = (20, 10)
         src_rect = rect_sized((10,10), (700,260))
-        #src_rect = rect_sized((180, 160), (100, 100))
         dst_rect = offset_rect(src_rect, main_image_pos)
-        #dst_rect = rect_sized((10, 340), (150, 150))
-        #dst_rect = rect_sized((200, 340), (100, 100))
-        #dst_rect = rect_sized((10, 10), (800, 790))
         background_image.draw(c, src_rect, dst_rect)
 
-def create_window():
+def create_window(): #USED to create and later readd buttons and views to the window
     window.add(view)
     window.add(join_button)
     window.add(join_list_button)
-    
-#    window.add(create_button)
-#    window.add(create_list_button)
     window.add(leave_button)
     window.add(window.input_field)
     window.add(window.output_field)
@@ -627,12 +462,10 @@ def create_window():
     window.add(window.input_alias)
     window.show()
 
-def remove_window():
+def remove_window(): #Used to remove all the buttons and views for refresh
     window.remove(view)
     window.remove(join_button)
     window.remove(join_list_button)
-#    window.remove(create_button)
-#    window.remove(create_list_button)
     window.remove(leave_button)
     window.remove(window.room_field)
     window.remove(window.input_field)
@@ -644,6 +477,9 @@ def remove_window():
     window.remove(window.input_alias)
     window.remove(mail_button)
     window.remove(window.alias)
+
+
+#WINDOW AND FIELD CREATION
 
 window = Window(title = "Chatcity!", 
     bounds = (50, 70, 810, 800),
@@ -690,17 +526,12 @@ window.alias = TestTextField(5, #consider condensing this with the alias textfie
 
 
 
-#clientgui()
+#THREAD CREATION AND APPLICATION RUNNING
 
 thread1 = clientThread(1, "Thread-1", 1)
-#thread2 = dummyThread(2, "Thread-2", 2)
-
 thread1.start()
-#thread2.start()
-
 create_window()
-
-
 application().run()
 
-#print "Exiting main thread"
+
+
