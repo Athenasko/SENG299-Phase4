4a701cd8e97aaa62f5e89e2f78b6892e70054e06
Austin Lee <austinlee.uvic@gmail.com>
2017-08-26 14:59:27 -0700

    Deleting to clr Master to merge w/ JuanTestBranch

--- client.py
+++
@@ -1,9 +0,0 @@
-#Socket Client segment provided by Caleb Shortt.
-import socket
-s = socket.socket()
-host = socket.gethostname()
-port = 9999
-address = (host, port)
-msg = 'test 123'
-s.connect(address)
-s.send(msg)
